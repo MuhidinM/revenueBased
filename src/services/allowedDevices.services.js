576c23c536746b3c702d6cb60cd8763dfc9d83a1
motumag <motumagishu27@gmail.com>
2023-02-10 14:54:43 +0300

    device services

--- src/services/allowedDevices.services.js
+++ src/services/allowedDevices.services.js
@@ -3,7 +3,7 @@ const fetchRegisteredDevicesUrl =
   "http://localhost:5000/registered/device/all/";
 
 const getAllRegisteredDevices = async () => {
-  console.log("calling endpoint");
+  // console.log("calling endpoint");
 
   return await axios.get(fetchRegisteredDevicesUrl).then((response) => {
     return response.data.allDevices;
