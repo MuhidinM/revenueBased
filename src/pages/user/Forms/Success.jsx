215691a47b78b83c44e07e365f6d9a1108af04be
Your Name <misbahmuhidin3@gmail.com>
2023-02-13 16:55:05 +0300

    final wizard

--- src/pages/user/Forms/Success.jsx
+++ src/pages/user/Forms/Success.jsx
@@ -5,38 +5,38 @@ function Success() {
     useContext(FormContext);
   return (
     <div className="font-medium">
-      <div className="grid gap-4 pt-8 sm:grid-cols-2 sm:gap-6">
+      <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
         {/* <div className="sm:col-span-2">Business Information</div> */}
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>Legal Name:</b> <br />
           {formData.lgname}
         </div>
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>Type of Incorporation:</b> <br />
           {formData.incorporation}
         </div>
 
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>Industry:</b> <br />
           {formData.industry}
         </div>
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>Category:</b> <br />
           {formData.category}
         </div>
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>Staff size:</b> <br />
           {formData.regstaffsizeion}
         </div>
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>Monthly Transaction Amount:</b> <br />
           {formData.transaction}
         </div>
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>Tin Number:</b> <br />
           {formData.tinno}
         </div>
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>Business Registration Number:</b> <br />
           {formData.bno}
         </div>
@@ -56,27 +56,27 @@ function Success() {
         <div className="divider sm:col-span-4">
           <h1>Comapany Address</h1>
         </div>
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>Region:</b> <br />
           {formData.region}
         </div>
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>City:</b> <br />
           {formData.bcity}
         </div>
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>Kifle Ketema:</b> <br />
           {formData.bkifleketema}{" "}
         </div>
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>Woreda:</b> <br />
           {formData.bworeda}{" "}
         </div>
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>Kebele:</b> <br />
           {formData.bkebele}{" "}
         </div>
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <b>House Number:</b> <br />
           {formData.hno}{" "}
         </div>
@@ -89,11 +89,16 @@ function Success() {
           <img
             className=""
             // src={m2SettingData.tradeLicense}
-            width={300}
-            height={400}
             alt="ID"
           />
         </div>
+        <button
+          type="submit"
+          // onClick={props.handleSubmit}
+          className="sm:col-span-4 text-white bg-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-primary dark:focus:ring-primary"
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
