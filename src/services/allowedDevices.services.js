e8ebad9e8a7366accf7fe30fc955083591fb6aaa
Amir Edris <amiredris@example.com>
2023-02-28 22:21:37 -0800

    some changes to set primary account, add account and select banks

--- src/services/allowedDevices.services.js
+++ src/services/allowedDevices.services.js
@@ -4,10 +4,12 @@ const deviceRegisterUrl = "http://localhost:5000/device/";
 
 const getAllRegisteredDevices = async (id) => {
   // console.log("calling endpoint");
+  console.log("User Id Is: ", id)
   return await axios
     .get(fetchRegisteredDevicesUrl + `all/${id}`)
     .then((response) => {
       // console.log("text" + response.data);
+      console.log(response.data);
       return response.data.allDevices;
     });
 };
