0818f358c3c567ae3b65f13f69f2dce30e6be61b
Amir Edris <amiredris@example.com>
2023-03-02 22:53:53 -0800

    agent services has been added

--- src/services/allowedDevices.services.js
+++ src/services/allowedDevices.services.js
@@ -4,7 +4,7 @@ const deviceRegisterUrl = "http://localhost:5000/device/";
 
 const getAllRegisteredDevices = async (id) => {
   // console.log("calling endpoint");
-  console.log("User Id Is: ", id)
+  console.log("User Id Is: ", id);
   return await axios
     .get(fetchRegisteredDevicesUrl + `all/${id}`)
     .then((response) => {
