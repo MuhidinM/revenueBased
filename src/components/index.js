9b6a1674c17f28d36a83cf528edb7aa370ac420c
Amir Edris <amiredris@example.com>
2023-03-09 22:46:15 -0800

    Account by Phone Number Ui has been implimented

--- src/components/index.js
+++ src/components/index.js
@@ -11,7 +11,9 @@ import { createTutorial } from "../store/actions/bank_accountAction";
 import { Provider } from "react-redux";
 import store from "../store/store";
 import Otp from "./Otp";
-
+import Input from "./Input";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 const MySwal = withReactContent(Swal);
 
 function ModalFire() {
@@ -72,40 +74,136 @@ function ModalFire() {
     }
   };
 
+  // const showFormModal = (values) => {
+  //   return new Promise((resolve, reject) => {
+  //     MySwal.fire({
+  //       title: "ADD ACCOUNTS",
+  //       html: (
+  //         <Provider store={store}>
+  //           <ModalForm
+  //             values={values}
+  //             // sendNameToParent={sendNameToParent}
+  //             onSubmit={(values) => {
+  //               console.log("Hello");
+  //               console.log(values);
+  //               // let crtVal = getcriterialVal();
+  //               // console.log(crtVal);
+  //               const {
+  //                 bankAccounts,
+  //                 message,
+  //                 accountMessage,
+  //                 criteriaValue,
+  //                 loading,
+  //               } = AccountListData;
+
+  //               console.log("Criteria Value 2 " + customerName2.current);
+  //               // console.log("Id is" + currentUser.id);
+  //               // console.log(customerName);
+  //               // dispatch(
+  //               //   createTutorial(
+  //               //     customerName2.current,
+  //               //     values.accountNumber,
+  //               //     values.bankName,
+  //               //     currentUser.id
+  //               //   )
+  //               // );
+
+  // const value = {
+  //   first: "",
+  //   second: "",
+  //   third: "",
+  //   fourth: "",
+  //   fifth: "",
+  //   sixth: "",
+  // };
+
+  //               BankAccountServices.sendOtp("0925825012").then((res) => {
+  //                 MySwal.fire({
+  //                   title: "",
+  //                   html: (
+  //                     <Otp
+  //                       values={value}
+  //                       onSubmit={(values1) => {
+  //                         console.log("Hello from the second swal");
+  //                         resolve(values);
+  //                         console.log(values.first);
+  //                         const otp =
+  //                           values1.first +
+  //                           values1.second +
+  //                           values1.third +
+  //                           values1.fourth +
+  //                           values1.fifth +
+  //                           values1.sixth;
+
+  //                         const confirmedOtp = BankAccountServices.confirmOtp(
+  //                           "0925825012",
+  //                           otp
+  //                         ).then((res) => {
+  //                           console.log("creating account");
+  //                           if (res.status === "success") {
+  //                             console.log("success is responded");
+  //                             console.log(criteriaValue);
+  //                             dispatch(
+  //                               createTutorial({
+  //                                 current: customerName2.current,
+  //                                 accountNumber: values.accountNumber,
+  //                                 bankName: values.bankName,
+  //                                 id: currentUser.user.user_id,
+  //                                 interpretResponse,
+  //                               })
+  //                             );
+  //                           }
+  //                         });
+  //                       }}
+  //                     ></Otp>
+  //                   ),
+  //                   onClose: () => reject(),
+  //                   showConfirmButton: false,
+  //                 });
+  //               });
+  //               resolve(values);
+
+  //               MySwal.close();
+  //               Swal.close();
+  //             }}
+  //             onCancel={() => {
+  //               Swal.close();
+  //             }}
+  //           />
+  //         </Provider>
+  //       ),
+
+  //       onClose: () => reject(),
+  //       showConfirmButton: false,
+  //     });
+  //   });
+  // };
+
+  // const showModal = () => {
+  //   showFormModal({
+  //     accountNumber: "",
+  //     bankName: "",
+  //   })
+  //     .then((values) => console.log(values))
+  //     .catch(() => console.log("Modal closed"));
+  // };
+
   const showFormModal = (values) => {
     return new Promise((resolve, reject) => {
+      const ValidationSchema = Yup.object().shape({
+        phoneNumber: Yup.string().required("Phone Number is required"),
+      });
       MySwal.fire({
-        title: "ADD ACCOUNTS",
+        title: "Add Account",
         html: (
-          <Provider store={store}>
-            <ModalForm
-              values={values}
-              // sendNameToParent={sendNameToParent}
+          <>
+            <Formik
+              initialValues={{ phoneNumber: "" }}
+              validationSchema={ValidationSchema}
+              isInitialValid={ValidationSchema.isValidSync(values)}
               onSubmit={(values) => {
-                console.log("Hello");
-                console.log(values);
-                // let crtVal = getcriterialVal();
-                // console.log(crtVal);
-                const {
-                  bankAccounts,
-                  message,
-                  accountMessage,
-                  criteriaValue,
-                  loading,
-                } = AccountListData;
-
-                console.log("Criteria Value 2 " + customerName2.current);
-                // console.log("Id is" + currentUser.id);
-                // console.log(customerName);
-                // dispatch(
-                //   createTutorial(
-                //     customerName2.current,
-                //     values.accountNumber,
-                //     values.bankName,
-                //     currentUser.id
-                //   )
-                // );
-
+                // console.log("Values Are:", values);
+                
                 const value = {
                   first: "",
                   second: "",
@@ -114,77 +212,151 @@ function ModalFire() {
                   fifth: "",
                   sixth: "",
                 };
+                MySwal.fire({
+                  title: "",
+                  html: (
+                    <Otp
+                      values={value}
+                      onSubmit={(values1) => {
+                        console.log("Hello from the second swal");
+                        resolve(values);
+                        console.log(values.first);
+                        const otp =
+                          values1.first +
+                          values1.second +
+                          values1.third +
+                          values1.fourth +
+                          values1.fifth +
+                          values1.sixth;
 
-                BankAccountServices.sendOtp("0925825012").then((res) => {
-                  MySwal.fire({
-                    title: "",
-                    html: (
-                      <Otp
-                        values={value}
-                        onSubmit={(values1) => {
-                          console.log("Hello from the second swal");
-                          resolve(values);
-                          console.log(values.first);
-                          const otp =
-                            values1.first +
-                            values1.second +
-                            values1.third +
-                            values1.fourth +
-                            values1.fifth +
-                            values1.sixth;
-
-                          const confirmedOtp = BankAccountServices.confirmOtp(
-                            "0925825012",
-                            otp
-                          ).then((res) => {
+                        const confirmedOtp = BankAccountServices.confirmOtp(
+                          "0925825012",
+                          otp
+                        )
+                          .then((res) => {
                             console.log("creating account");
                             if (res.status === "success") {
                               console.log("success is responded");
                               console.log(criteriaValue);
-                              dispatch(
-                                createTutorial({
-                                  current: customerName2.current,
-                                  accountNumber: values.accountNumber,
-                                  bankName: values.bankName,
-                                  id: currentUser.user.user_id,
-                                  interpretResponse,
-                                })
-                              );
+
+                              // dispatch(
+                              //   createTutorial({
+                              //     current: customerName2.current,
+                              //     accountNumber: values.accountNumber,
+                              //     bankName: values.bankName,
+                              //     id: currentUser.user.user_id,
+                              //     interpretResponse,
+                              //   })
+                              // );
                             }
+                          })
+                          .catch((err) => {
+                            const bankAccounts = [1, 2];
+                            const renderList = bankAccounts.map(
+                              (item, index) => (
+                                <div class="flex items-center mb-4">
+                                  <input
+                                    id="default-checkbox"
+                                    type="checkbox"
+                                    value=""
+                                    class="w-4 h-4 checkbox checkbox-primary border-gray-300 rounded dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
+                                  />
+                                  <label
+                                    for="default-checkbox"
+                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                  >
+                                    {item}
+                                  </label>
+                                </div>
+                              )
+                            );
+
+                            MySwal.fire({
+                              title: "Choose Account",
+                              html: <div className="">{renderList}</div>,
+                              onClose: () => reject(),
+                              onCancel: () => Swal.close(),
+                              showConfirmButton: false,
+                              showCancelButton: false,
+                              confirmButtonColor: "#01AFEF",
+                            });
                           });
-                        }}
-                      ></Otp>
-                    ),
-                    onClose: () => reject(),
-                    showConfirmButton: false,
-                  });
+                      }}
+                    ></Otp>
+                  ),
+                  onClose: () => reject(),
+                  showConfirmButton: false,
                 });
-                resolve(values);
-
-                MySwal.close();
-                Swal.close();
               }}
-              onCancel={() => {
-                Swal.close();
-              }}
-            />
-          </Provider>
+            >
+              {(formik) => (
+                <>
+                  <form onSubmit={formik.handleSubmit}>
+                    <div className="grid gap-2 sm:grid-cols-5 sm:gap-6 sm:mb-5">
+                      <div className="col-span-3">
+                        <span className="text-sm link-error">
+                          <ErrorMessage name="phoneNumber" />
+                        </span>
+                        <Input
+                          label="pno"
+                          title="Phone Number"
+                          type="number"
+                          name="phoneNumber"
+                          id="phoneNumber"
+                          place="0987654321"
+                          value={formik.values.phoneNumber}
+                          handleChange={formik.handleChange}
+                          // fetchName={queryCustomerName}
+                          //   handleChange={handleChange}
+                        />
+                      </div>
+                      <div className="col-span-2 mt-5">
+                        <button
+                          type="submit"
+                          onClick={formik.handleSubmit}
+                          className="swal2-confirm swal2-styled btn-primary"
+                        >
+                          verify
+                        </button>
+                      </div>
+                    </div>
+                    {/* <button
+                      type="submit"
+                      onSubmit={onSubmit}
+                      className="swal2-confirm swal2-styled"
+                    >
+                      OK
+                    </button>
+                    <button
+                      onClick={onCancel}
+                      className="swal2-cancel swal2-styled"
+                    >
+                      Cancel
+                    </button> */}
+                  </form>
+                </>
+              )}
+            </Formik>
+          </>
         ),
-
         onClose: () => reject(),
+        onCancel: () => Swal.close(),
         showConfirmButton: false,
+        showCancelButton: false,
+        confirmButtonColor: "#01AFEF",
       });
     });
   };
 
   const showModal = () => {
     showFormModal({
-      accountNumber: "",
-      bankName: "",
+      name: "",
+      url: "",
     })
       .then((values) => console.log(values))
       .catch(() => console.log("Modal closed"));
   };
+
   return (
     <button
       type="button"
