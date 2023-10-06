f05e55eddbdc4d41a418fca6137b12c714b94051
Yado3230 <yaredbest81@gmail.com>
2023-10-06 09:27:19 +0300

    final

--- src/utils/API.jsx
+++ src/utils/API.jsx
@@ -2,21 +2,21 @@ import axios from "axios";
 import store from "./../store/store";
 
 // Create a new instance of axios with the token in the header
-export const LOGIN_NODE_API = axios.create({
-  baseURL: "http://10.1.177.130:5004/api",
-});
-
-export const NODE_API = axios.create({
-  baseURL: "http://10.1.177.130:5004/api",
-});
-
 // export const LOGIN_NODE_API = axios.create({
-//   baseURL: "http://192.168.14.154:5000/api",
+//   baseURL: "http://10.1.177.130:5000/api",
 // });
 
 // export const NODE_API = axios.create({
-//   baseURL: "http://192.168.14.154:5000/api",
+//   baseURL: "http://10.1.177.130:5000/api",
 // });
+
+export const LOGIN_NODE_API = axios.create({
+  baseURL: "https://192.168.14.62:5000/api",
+});
+
+export const NODE_API = axios.create({
+  baseURL: "https://192.168.14.62:5000/api",
+});
 
 // Request interceptor to update Authorization header with the token
 NODE_API.interceptors.request.use((config) => {
@@ -31,15 +31,3 @@ NODE_API.interceptors.request.use((config) => {
 
   return config;
 });
-
-// // Use LOGIN_NODE_API instance to make requests
-// // For example:
-// LOGIN_NODE_API.get("/example").then((response) => {
-//   // Handle the response
-// });
-
-// // Use NODE_API instance to make requests
-// // For example:
-// NODE_API.get("/example").then((response) => {
-//   // Handle the response
-// });
