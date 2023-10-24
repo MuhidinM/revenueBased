a7841fad7d9bb3d2212e7d1be91cc46298974bbb
MuhidinM <misbahmuhidin3@gmail.com>
2023-10-24 09:48:13 +0300

    discription row reduesed

--- src/pages/user/AddInventory.jsx
+++ src/pages/user/AddInventory.jsx
@@ -345,7 +345,7 @@ function AddInventory({ onSubmit, values, onCancel }) {
                   id="description"
                   value={formik.values.description}
                   onChange={formik.handleChange}
-                  rows="8"
+                  rows="4"
                   className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary focus:border-primary dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary"
                   placeholder="Your description here"
                 ></textarea>
